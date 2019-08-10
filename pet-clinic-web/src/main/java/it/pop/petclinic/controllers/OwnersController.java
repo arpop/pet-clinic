b31db4fd85c7d4dfa37412bfb81b6bd2a3293f5a
Andrei Pop <pop.andrei@yahoo.com>
2019-08-10 19:40:53 +0200

    listing owners on page, closes #9

--- pet-clinic-web/src/main/java/it/pop/petclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/it/pop/petclinic/controllers/OwnersController.java
@@ -1,14 +1,23 @@
 package it.pop.petclinic.controllers;
 
+import it.pop.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnersController {
-    
+
+    private final OwnerService ownerService;
+
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"" , "/", "/index", "/index.html"})
-    public String listVets() {
-        return "/owners/index";
+    public String listVets(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
+        return "owners/index";
     }
 }
