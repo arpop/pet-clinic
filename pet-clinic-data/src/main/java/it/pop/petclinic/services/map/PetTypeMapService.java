8ab64ec7c8bbe51c4e7f8e2473d581f0bf8f1812
Andrei Pop <pop.andrei@yahoo.com>
2019-08-12 18:21:42 +0200

    Map Service implementation for PetType, closes #25

---
+++ pet-clinic-data/src/main/java/it/pop/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,36 @@
+package it.pop.petclinic.services.map;
+
+import it.pop.petclinic.model.PetType;
+import it.pop.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType entity) {
+        super.delete(entity);
+    }
+
+    @Override
+    public PetType save(PetType entity) {
+        return super.save(entity);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
