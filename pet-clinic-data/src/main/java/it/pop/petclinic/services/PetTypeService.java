8eef536ffb5052f1c9ee74f3b1813fd5ba66daf9
Andrei Pop <pop.andrei@yahoo.com>
2019-08-12 18:30:56 +0200

    Loaded pet types, closes #28

---
+++ pet-clinic-data/src/main/java/it/pop/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package it.pop.petclinic.services;
+
+import it.pop.petclinic.model.PetType;
+import it.pop.petclinic.services.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
