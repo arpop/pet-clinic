0f7685465b8b82f106dfa6b7dacbfc199c1cf069
Andrei Pop <pop.andrei@yahoo.com>
2019-10-12 11:28:16 +0200

    added jpa implementation of the visit service (closes #43)

---
+++ pet-clinic-data/src/main/java/it/pop/petclinic/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,47 @@
+package it.pop.petclinic.services.springdatajpa;
+
+import it.pop.petclinic.model.Visit;
+import it.pop.petclinic.repositories.VisitRepository;
+import it.pop.petclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit entity) {
+        return visitRepository.save(entity);
+    }
+
+    @Override
+    public void delete(Visit entity) {
+        visitRepository.delete(entity);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
