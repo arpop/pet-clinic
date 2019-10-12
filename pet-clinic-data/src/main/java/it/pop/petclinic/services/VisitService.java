3f9e11338bb64ad65c0435e48a10a0f5d02c58ef
Andrei Pop <pop.andrei@yahoo.com>
2019-10-12 11:15:41 +0200

    added visit service and map implementation of it (closes #42)

---
+++ pet-clinic-data/src/main/java/it/pop/petclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package it.pop.petclinic.services;
+
+import it.pop.petclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/it/pop/petclinic/services/map/VisitServiceMap.java
@@ -0,0 +1,37 @@
+package it.pop.petclinic.services.map;
+
+import it.pop.petclinic.model.Visit;
+import it.pop.petclinic.services.VisitService;
+
+import javax.swing.text.html.parser.Entity;
+import java.util.Set;
+
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit entity) {
+        super.delete(entity);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getOwner().getId() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
